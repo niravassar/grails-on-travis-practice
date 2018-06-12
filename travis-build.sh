f999fdb07613b617fdb4c813bb1be39b534f1d91
niravassar <assarn@objectcomputing.com>
2018-06-12 16:47:10 -0500

    yml with sh file

--- travis-build.sh
+++ travis-build.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+
+EXIT_STATUS=0
+
+echo "************ Gradle assemble.... *************"
+./gradlew assemble || EXIT_STATUS=$?
+
+echo "************ Gradle check.... *************** "
+./gradlew check || EXIT_STATUS=$?
+
+exit $EXIT_STATUS
